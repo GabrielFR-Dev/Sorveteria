2c27a4315757e99a16e673525b330f59fdc9bf91
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-01 15:31:37 -0300

    Terminei o html do rodape
    integrei o Topo e o Rodape em todas as páginas utilizando o documento layout.js

--- sorveteria-react/src/app/layout.js
+++ sorveteria-react/src/app/layout.js
@@ -12,8 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        
+        <Topo/>
         {children}
+        <Rodape/>
       </body>
     </html>
   );
--- sorveteria-react/src/componentes/Rodape/index.jsx
+++ sorveteria-react/src/componentes/Rodape/index.jsx
@@ -1,1 +1,37 @@
+import Image from "next/image";
+import Logo from "/public/logo.png";
+import estilos from "./Rodape.module.css";
 
+export default function Rodape () {
+    return (
+        <footer>
+            <Image className={estilos.logo} src={Logo} alt="Logo"/>
+            <div className={estilos.container_divs}>
+                
+                <div className={estilos.div}>
+                    <h4>ENDEREÇO</h4>
+                    <p>
+                        Av. Bernardinho de campos 98 são paulo, SP 12345-678
+                    </p>
+                </div>
+
+                <div className={estilos.div}>
+                    <h4>CONTATO</h4>
+                    <p>
+                        info@meusite.com
+                        tel(xx) xxxxx-xxxx
+                    </p> 
+                </div>
+
+                <div className={estilos.div}>
+                    <h4>HORÁRIOS</h4>
+                    <p>
+                        ABERTO TODOS OS DIAS 10:00 - 22:00
+                    </p> 
+                </div>
+            </div>
+
+            <p>Gelateria. Orgulhosamente desenvolvido por 'Gabriel ferreira'</p>
+        </footer>
+    )
+};
