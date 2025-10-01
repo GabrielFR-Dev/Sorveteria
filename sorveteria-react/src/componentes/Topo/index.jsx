081acd41a997808112f1acf76b1b217285ba8557
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-01 16:08:11 -0300

    estilizei o css do topo
    coloquei a paleta de cores no css global e editei alguns códigos no html do topo.

--- sorveteria-react/src/componentes/Topo/index.jsx
+++ sorveteria-react/src/componentes/Topo/index.jsx
@@ -1,22 +1,24 @@
 import Estilos from "./Topo.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import Logo from  "/public/logo.png";
+import Logo from "/public/logo.png";
 
 
-export default function Topo () {
+export default function Topo() {
     return (
-        
+
         <header className={Estilos.container_topo}>
-             
+
+            <div className={Estilos.topo}>
                 <Image className={Estilos.img} src={Logo} alt="Logo" />
 
                 <nav className={Estilos.navegacao}>
-                    <Link href="/">Home</Link>
-                    <Link href="/Sabores">Sabores</Link>
-                    <Link href="/Sobre">Sobre</Link>
+                    <Link href="/" className={Estilos.link}>Home</Link>
+                    <Link href="/Sabores" className={Estilos.link}>Sabores</Link>
+                    <Link href="/Sobre" className={Estilos.link}>Sobre</Link>
                 </nav>
-                
+            </div>
+
         </header>
 
     );
